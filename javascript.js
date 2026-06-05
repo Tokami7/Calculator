1330514c2029e1f3a30831769fe8829cecf17508
Tom <thomas.mant@hotmail.com>
2026-06-05 19:01:31 +0100

    Add standard calculator functionality create functions to update number and operator variables and event listeners to call the functions

--- javascript.js
+++ javascript.js
@@ -3,6 +3,25 @@ let secondNumber
 let operator
 let result
 
+const display = document.querySelector(".display");
+const clearBtn = document.querySelector(".clear");
+const zeroBtn = document.querySelector(".zero");
+const oneBtn = document.querySelector(".one");
+const twoBtn = document.querySelector(".two");
+const threeBtn = document.querySelector(".three");
+const fourBtn = document.querySelector(".four");
+const fiveBtn = document.querySelector(".five");
+const sixBtn = document.querySelector(".six");
+const sevenBtn = document.querySelector(".seven");
+const eightBtn = document.querySelector(".eight");
+const nineBtn = document.querySelector(".nine");
+const pointBtn = document.querySelector(".point");
+const equalsBtn = document.querySelector(".equals");
+const addBtn = document.querySelector(".add");
+const subtractBtn = document.querySelector(".subtract");
+const multiplyBtn = document.querySelector(".multiply");
+const divideBtn = document.querySelector(".divide");
+
 function add(a,b) {
     return a + b;
 };
@@ -30,3 +49,52 @@ function operate(num1,op,num2) {
         result = divide(num1,num2);
     };
 };
+
+function updateNumbers(num) {
+    if (firstNumber === undefined) {
+        firstNumber = num;
+        display.textContent = firstNumber;
+    } else if (firstNumber !== undefined && operator === undefined){
+        firstNumber = Number("" + firstNumber + num);
+        display.textContent = firstNumber;
+    } else if (secondNumber === undefined) {
+        secondNumber = num;
+        display.textContent = secondNumber;
+    } else {
+        secondNumber = Number("" + secondNumber + num);
+        display.textContent = secondNumber;
+    };
+
+};
+
+function updateOperator(op) {
+    if (operator === undefined) {
+        operator = op;
+    } else {
+        operate(firstNumber,operator,secondNumber);
+        display.textContent = result;
+        firstNumber = result;
+        secondNumber = undefined;
+        operator = op;
+    };
+}
+
+zeroBtn.addEventListener("click", () => {updateNumbers(0)});
+oneBtn.addEventListener("click", () => {updateNumbers(1)});
+twoBtn.addEventListener("click", () => {updateNumbers(2)});
+threeBtn.addEventListener("click", () => {updateNumbers(3)});
+fourBtn.addEventListener("click", () => {updateNumbers(4)});
+fiveBtn.addEventListener("click", () => {updateNumbers(5)});
+sixBtn.addEventListener("click", () => {updateNumbers(6)});
+sevenBtn.addEventListener("click", () => {updateNumbers(7)});
+eightBtn.addEventListener("click", () => {updateNumbers(8)});
+nineBtn.addEventListener("click",  () => {updateNumbers(9)});
+clearBtn.addEventListener("click", () => firstNumber = secondNumber = operator = result = display.textContent = undefined);
+addBtn.addEventListener("click", () => {updateOperator("+")});
+subtractBtn.addEventListener("click", () => {updateOperator("-")});
+multiplyBtn.addEventListener("click", () => {updateOperator("*")});
+divideBtn.addEventListener("click", () => {updateOperator("/")});
+equalsBtn.addEventListener("click", () => {
+    operate(firstNumber,operator,secondNumber);
+    display.textContent = result;
+});
