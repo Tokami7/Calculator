f91f29dd6600b3d393cf46a1c4d6bbb74db95a30
Tom <thomas.mant@hotmail.com>
2026-06-07 12:42:54 +0100

    Add keyboard support

--- javascript.js
+++ javascript.js
@@ -145,3 +145,100 @@ pointBtn.addEventListener("click", () => {
         display.textContent = secondNumber;
     };
 });
+
+document.addEventListener("keydown", (e) => {
+    if (event.defaultPrevented) {
+        return;
+    };
+    
+    switch (e.key){
+        case "0":
+            updateNumbers(0);
+            break;
+        case "1":
+            updateNumbers(1);
+            break;
+        case "2":
+            updateNumbers(2);
+            break;
+        case "3":
+            updateNumbers(3);
+            break;
+        case "4":
+            updateNumbers(4);
+            break;
+        case "5":
+            updateNumbers(5);
+            break;
+        case "6":
+            updateNumbers(6);
+            break;
+        case "7":
+            updateNumbers(7);
+            break;
+        case "8":
+            updateNumbers(8);
+            break;
+        case "9":
+            updateNumbers(9);
+            break;
+        case "Escape":
+            firstNumber = secondNumber = operator = result = display.textContent = "";
+            break;
+        case "Backspace":
+            if (firstNumber === "" && secondNumber === "") {
+        
+            } else if (operator !== "" && secondNumber === ""){
+        
+            } else if (secondNumber === "") {
+                firstNumber = firstNumber.slice(0, -1);
+                display.textContent = firstNumber;
+            } else {
+                secondNumber = secondNumber.slice(0, -1);
+                display.textContent = secondNumber;
+            };
+            break;
+        case "+":
+            updateOperator("+");
+            break;
+        case "-":
+            updateOperator("-");
+            break;
+        case "*":
+            updateOperator("*");
+            break;
+        case "/":
+            updateOperator("/");
+            break;
+        case "=":
+            if (firstNumber === "" || secondNumber === "" || operator === "") {
+
+            } else {
+                operate(firstNumber,operator,secondNumber);
+                display.textContent = result;
+                firstNumber = secondNumber = operator = result = "";
+            };
+            break;
+        case ".":
+            if (firstNumber === "" && operator === "" && secondNumber === "") {
+                firstNumber = "0.";
+                display.textContent = firstNumber;
+            } else if (firstNumber.includes(".") && operator === "" && secondNumber === "") {
+
+            } else if (operator === "" && secondNumber === "") {
+                firstNumber += ".";
+                display.textContent = firstNumber;
+            } else if (secondNumber === "") {
+                secondNumber = "0.";
+                display.textContent = secondNumber;
+            } else if (secondNumber.includes(".")) {
+
+            } else if (secondNumber !== "") {
+                secondNumber += ".";
+                display.textContent = secondNumber;
+            };
+            break;
+    };
+
+    event.preventDefault();
+},true);
